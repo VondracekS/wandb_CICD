6e29b2edbdec21d6cf1c2bb9ee964bf7f5900a61
Stepan Vondracek <112405666+VondracekS@users.noreply.github.com>
2023-04-08 10:38:32 +0200

    Update ci.py

--- ci.py
+++ ci.py
@@ -1,4 +1,4 @@
 import wandb
 
 print(f"Current version of the wnadb package is {wandb.__version__}")
-assert wandb.__version__ == '1.0.0', print(f"Expencted 1.0.0 version, got {wandb.__version__}")
+assert wandb.__version__ == '1.0.0', print(f"Expencted 1.0.1 version, got {wandb.__version__}")
